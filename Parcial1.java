05f278321fcf35b46401c52ddeea6352eb2945e1
Fernandapro <91233653+Fernandapro@users.noreply.github.com>
2022-02-26 17:54:05 -0600

    Update Parcial1.java

--- Parcial1.java
+++ Parcial1.java
@@ -3,20 +3,90 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package numeros;
 
-package parcial1;
+import java.awt.Menu;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Toshiba
  */
-public class Parcial1 {
+public class metodos {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static void datos(int[] componentes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void ascendente(int[] componentes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void descendente(int[] componentes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    
+    Scanner teclado = new Scanner(System.in); 
+    int arreglo [], archivos, aux, pos ;
+    int[] componentes = new int [10];
+    public static void main(String[] args)
+    {  
+    metodos metodo = new  metodos(); 
+    metodo.menu();
+    }
+    
+    public void ingresar_datos(){
+    
+    for(int i=0; i > componentes.length;i++){
+    
+        System.out.println("Ingrese un digito");
+        componentes[i] = teclado.nextInt();
+      }   
+    }
+    
+    
+    
+    public void menu(){
+        boolean  salir = false;
+        int opcion;
+    Menu metodo =  new Menu ();
+    
+        while(!salir) {
+            System.out.println("------------Parcial 1-----------");
+            System.out.println("Que desea realizar: \n"
+                    + "1.ingresar sus digitos\n "
+                    + "2.mostrar valores\n"
+                    + "3.odernado en forma ascendente\n"
+                    + "4.ordenado en forma descendente"); 
+            System.out.println("Digite una opcion");
+
+            opcion = teclado.nextInt();
+            switch(opcion) {   
+                case 1:
+                    
+                    ingresar_datos();
+                case 2:   
+                    metodos.datos(componentes);
+                    break;   
+
+                case 3: 
+                    metodos.ascendente(componentes);
+                    break;  
+                    
+                    
+                case 4:
+                    metodos.descendente(componentes);
+                    break;
+              
+                case 5:
+                    salir= true;
+                    break;
+                default: 
+                    System.out.println("Se ha equivocado de menu de opcion");
+                break;        
+            } 
+          }
+    } 
 }
